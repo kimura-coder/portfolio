6d1f834f41ba6c493ec143a796629eedf605269f
kimura-coder <kimuraalfred@gmail.com>
2025-05-23 14:36:24 +0300

    updated vite.config.js

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kimura-coder.github.io/my-portfolio/',
+  base: 'https://kimura-coder.github.io/portfolio/',
 })
